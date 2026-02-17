397e0d5312c730671f89995f09e332ad439c3464
DevObaloluwa <securedbyoba@gmail.com>
2026-02-17 08:10:49 +0100

    feat: Add go_router import for navigation.

--- lib/src/features/client/presentation/client_products_screen.dart
+++ lib/src/features/client/presentation/client_products_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import '../../../common/styles/app_colors.dart';
 import '../../client/data/product_repository.dart';
 import '../../../common/models/product.dart';
