ce94065fa507f16f8dc99d438370bca3014f1b61
DevObaloluwa <securedbyoba@gmail.com>
2026-02-27 16:47:31 +0100

    Update production API URL

--- lib/src/common/config/app_config.dart
+++ lib/src/common/config/app_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class AppConfig {
   static const String devBaseUrl = 'http://localhost:8080/api/v1';
-  static const String prodBaseUrl = 'http://api.gopickup.com/api/v1'; // Change this to your real domain
+  static const String prodBaseUrl = 'https://backend.gopickup.com.ng/api/v1';
 
   static String get baseUrl {
     if (kIsWeb) {
